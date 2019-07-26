49eb8fcf53ebe9c8260e32ae7642109d6169741a
Vanessa Harbeck <vanessa.harbeck@web.de>
2019-07-26 14:19:34 +0200

    Setup Styleguide

---
+++ src/components/Headline.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+const Headline = styled.h1`
+  font-size: 30px;
+`;
+
+export default Headline;
---
+++ src/misc/GlobalStyle.js
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+body {
+   background: #FDF7F5;
+   margin: 0px;
+   font-family: 'Nunito', sans-serif;
+}
+`;
+
+export default GlobalStyle;
