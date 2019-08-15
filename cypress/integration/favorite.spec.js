c5ddd60acaf509a9f1fb5f9137f8b6d6af9b84bf
Vanessa Harbeck <vanessa.harbeck@web.de>
2019-08-15 10:56:54 +0200

    Fix Testing with Login

--- cypress/integration/favorite.spec.js
+++ cypress/integration/favorite.spec.js
@@ -1,6 +1,14 @@
 describe("Favorite", () => {
   beforeEach(() => {
-    cy.visit("http://localhost:3000/favorite");
+    cy.visit("http://localhost:3000/login");
+    cy.get('[data-cy="input-username"]').type("Nessa");
+    cy.get('[data-cy="input-password"]').type("1234");
+
+    cy.get('[data-cy="submit-button"]').click();
+    cy.location("pathname").should("be", "feed");
+
+    cy.get('[data-cy="nav-favorite"]').click();
+    cy.location("pathname").should("be", "favorite");
   });
   it("navigates to feed page on click", () => {
     cy.get('[data-cy="nav-favorite-feed"]').click();
--- cypress/integration/profile.spec.js
+++ cypress/integration/profile.spec.js
@@ -1,6 +1,14 @@
 describe("Profile", () => {
   beforeEach(() => {
-    cy.visit("http://localhost:3000/profile");
+    cy.visit("http://localhost:3000/login");
+    cy.get('[data-cy="input-username"]').type("Nessa");
+    cy.get('[data-cy="input-password"]').type("1234");
+
+    cy.get('[data-cy="submit-button"]').click();
+    cy.location("pathname").should("be", "feed");
+
+    cy.get('[data-cy="nav-profile"]').click();
+    cy.location("pathname").should("be", "profile");
   });
   it("navigates to login page on click", () => {
     cy.get('[data-cy="nav-logout"]').click();
